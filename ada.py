7e3919f1380fdcbf8009b56457488064a3ede0e5
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-03-26 17:57:20 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,9 +1,14 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    print(f"{first_name.lower()} {last_name.lower()}")    
-    print(f"{first_name.title()} {last_name.title()}")
-    print(f"{first_name.upper()} {last_name.upper()}")
-    print(f"\t{first_name.lower()} {last_name.lower()}")
+    output1 = f"{first_name.lower()} {last_name.lower()}"
+    output2 = f"{first_name.title()} {last_name.title()}"
+    output3 = f"{first_name.upper()} {last_name.upper()}"
+    output4 = f"\t{first_name.lower()} {last_name.lower()}"
+
+    print(output1)
+    print(output2)
+    print(output3)
+    print(output4)
 
 ada()
