e87f25485a888a8ae41dd79f7e0ef2784c1593f6
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-03-26 17:52:30 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,7 +1,15 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    print(f"The result of {x} comes first in the dictionary than {y} is {x<y}")
-    print(f"The result of {y} comes first in the dictionary than {x} is {y<x}")
+resultado_x_antes_y = x < y
+resultado_y_antes_x = y < x
+
+output1= f"The result of {x} comes first in the dictionary than {y} is {resultado_x_antes_y}"
+
+print(output1)
+
+output2= f"The result of {y} comes first in the dictionary than {x} is {resultado_y_antes_x}"
+
+print(output2)
 
 earth()
